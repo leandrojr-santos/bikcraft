f6beab6b44960f68fb0cf2fe66196fe66bfa879a
Leandro Junior Inocencio dos Santos <jrleandro321@gmail.com>
2022-08-15 05:28:36 -0300

    perguntas-frequentes-javascript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Ativar menu
 const links = document.querySelectorAll('.header-menu a')
 
 function ativarLink(link){ //pega o url e o caminho do href
@@ -10,3 +11,37 @@ function ativarLink(link){ //pega o url e o caminho do href
 }
 
 links.forEach(ativarLink)
+
+//Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search)// selecionou parametros do url e transformou em um array
+
+function ativarProduto(parametro){//percorreu parametros do array e marcou o elemento com id do parametro
+  const elemento = document.getElementById(parametro)
+ if(elemento){
+  elemento.checked = true
+ }
+}
+
+parametros.forEach(ativarProduto)
+
+//perguntas frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button')
+
+function ativarPergunta(event){
+  const pergunta = event.currentTarget
+  const controls = pergunta.getAttribute('aria-controls')
+  const resposta = document.getElementById(controls)
+
+  resposta.classList.toggle('ativa')
+  const ativa = resposta.classList.contains("ativa")
+  pergunta.setAttribute("aria-expandend", "true")
+  
+}
+
+function eventosPerguntas(pergunta){
+  pergunta.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas)
